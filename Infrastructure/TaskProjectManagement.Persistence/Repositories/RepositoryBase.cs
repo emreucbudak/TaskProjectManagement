e1b0c919b462bef6ce38af9a31121d6a107fe6c8
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-07-06 09:24:36 +0300

    RepositoryBase Eklendi

---
+++ Infrastructure/TaskProjectManagement.Persistence/Repositories/RepositoryBase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskProjectManagement.Application.Interfaces.Repositories;
+
+namespace TaskProjectManagement.Persistence.Repositories
+{
+    public class RepositoryBase<T> : IRepositoryBase<T> where T : class, new()
+    {
+        public Task Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
