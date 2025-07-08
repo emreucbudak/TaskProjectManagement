dc45d135299ce352e124f2ac03192ffebd2673bc
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-07-08 23:53:43 +0300

    DBCONTEXT EKLENDİ

--- Infrastructure/TaskProjectManagement.Persistence/ApplicationDbContext/ApplicationDbContext.cs
+++ Infrastructure/TaskProjectManagement.Persistence/ApplicationDbContext/ApplicationDbContext.cs
@@ -1,17 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using TaskProjectManagement.Domain.Entities;
 
 namespace TaskProjectManagement.Persistence.ApplicationDbContext
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext()
+        public ApplicationDbContext(DbContextOptions options) : base(options)
         {
-
         }
+        public DbSet<Roles> Roles {  get; set; }
+        public DbSet<Demand> Demands { get; set; }
+        public DbSet<Mission> Missions { get; set; }
+        public DbSet<MissionStatus> MissionStatus { get; set; }
+        public DbSet<Notifications> Notifications { get; set; }
+        public DbSet<SubTask> SubTasks { get; set; }
+        public DbSet<SubTaskStatus> SubTasksStatus { get; set; }
+        public DbSet<Team> Team { get; set; }
+        public DbSet<TeamMember> TeamMember { get; set; }
+        public DbSet<TeamStatus> TeamStatus { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserNotification> UsersNotification { get; set; }
     }
 }
