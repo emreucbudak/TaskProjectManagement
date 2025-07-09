6dffbcecb81e65352bee7bfb17629da170eca945
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-07-09 09:12:29 +0300

    Notification interface'i eklendi

---
+++ Core/TaskProjectManagement.Application/Interfaces/Repositories/INotificationRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskProjectManagement.Domain.Entities;
+
+namespace TaskProjectManagement.Application.Interfaces.Repositories
+{
+    public interface INotificationRepository
+    {
+        Task AddNotification(Notifications ntfc);
+        Task RemoveNotification(Notifications ntfc);
+        Task UpdateNotification (Notifications ntfc);
+        Task<IEnumerable<Notifications>> GetAllNotifications();
+        Task<Notifications> GetNotificationsById(int id);
+    }
+}
