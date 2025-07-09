bbb1ed33b092791dfe1c3461c59a9ef057ab2072
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-07-09 09:19:41 +0300

    SubTask Eklendi

---
+++ Core/TaskProjectManagement.Application/Interfaces/Repositories/ISubTaskRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskProjectManagement.Domain.Entities;
+
+namespace TaskProjectManagement.Application.Interfaces.Repositories
+{
+    public interface ISubTaskRepository
+    {
+        Task AddSubtask (SubTask task);
+        Task DeleteSubtask (SubTask task);
+        Task UpdateSubtask (SubTask task);
+        Task<IEnumerable<SubTask>> GetAllSubtasks ();
+        Task<SubTask> GetSubTaskById (int id);
+    }
+}
