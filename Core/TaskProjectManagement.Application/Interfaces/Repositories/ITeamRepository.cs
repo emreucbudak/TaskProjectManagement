d73d5c0cd7920b4369b0d12a79815e851c8f3f00
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-07-09 09:22:47 +0300

    Team interface eklendi

---
+++ Core/TaskProjectManagement.Application/Interfaces/Repositories/ITeamRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskProjectManagement.Domain.Entities;
+
+namespace TaskProjectManagement.Application.Interfaces.Repositories
+{
+    public interface ITeamRepository
+    {
+        Task AddTeam(Team team);
+        Task RemoveTeam(Team team);
+        Task UpdateTeam(Team team);
+        Task<IEnumerable<Team>> GetAllTeams();
+        Task<Team> GetTeamById(int id);
+    }
+}
