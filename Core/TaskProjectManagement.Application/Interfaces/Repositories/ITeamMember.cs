f7b29bf737f243d37225447d737348a74b4cc841
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-07-09 10:28:27 +0300

    Team Members Eklendi

---
+++ Core/TaskProjectManagement.Application/Interfaces/Repositories/ITeamMember.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskProjectManagement.Domain.Entities;
+
+namespace TaskProjectManagement.Application.Interfaces.Repositories
+{
+    public interface ITeamMember
+    {
+        Task AddMember(int teamID, int userID);
+        Task DeleteMember(int teamID, int userID);
+        Task<TeamMember> GetMember(int teamID, int userID);
+        Task<IEnumerable<TeamMember>> GetMembers(int teamID);
+
+    }
+}
