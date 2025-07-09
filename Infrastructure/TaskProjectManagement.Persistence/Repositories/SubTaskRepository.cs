8490ef7b5aa312689246a3dec15db4423e4c382c
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-07-09 22:33:55 +0300

    SubTask Repository eklendi

---
+++ Infrastructure/TaskProjectManagement.Persistence/Repositories/SubTaskRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskProjectManagement.Application.Interfaces.Repositories;
+using TaskProjectManagement.Domain.Entities;
+using TaskProjectManagement.Persistence.ProjectDbContext;
+
+namespace TaskProjectManagement.Persistence.Repositories
+{
+    public class SubTaskRepository : RepositoryBase<SubTask>, ISubTaskRepository
+    {
+        public SubTaskRepository(ApplicationDbContext db) : base(db)
+        {
+        }
+
+        public async Task AddSubtask(SubTask task)
+        {
+            await AddObj(task);
+        }
+
+        public async Task DeleteSubtask(SubTask task)
+        {
+            await DeleteObj(task);
+        }
+
+        public async Task<IEnumerable<SubTask>> GetAllSubtasks()
+        {
+            return await GetAllObj(false);
+        }
+
+        public async Task<SubTask> GetSubTaskById(int id)
+        {
+            return await GetByIdObj(b=> b.SubTaskId == id);
+        }
+
+        public async Task UpdateSubtask(SubTask task)
+        {
+            await UpdateObj(task);
+        }
+    }
+}
