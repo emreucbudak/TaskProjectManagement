89f1801c00cc30a99dc6daa367fa7b43c11da22e
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-07-10 00:50:43 +0300

    Mission Service Eklendi

---
+++ Core/TaskProjectManagement.Application/Interfaces/Services/IMissionService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskProjectManagement.Application.Interfaces.Services
+{
+    public interface IMissionService
+    {
+    }
+}
