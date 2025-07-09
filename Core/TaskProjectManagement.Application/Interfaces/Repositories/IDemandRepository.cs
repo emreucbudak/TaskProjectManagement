f2036d536aff28a5142725b4c2628f0fed08d6e0
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-07-09 09:00:16 +0300

    DemandRepository eklendi

---
+++ Core/TaskProjectManagement.Application/Interfaces/Repositories/IDemandRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskProjectManagement.Domain.Entities;
+
+namespace TaskProjectManagement.Application.Interfaces.Repositories
+{
+    public interface IDemandRepository
+    {
+        Task AddDemand(Demand dem);
+        Task DeleteDemand(Demand demand);
+        Task UpdateDemand(Demand demand);
+        Task<Demand> GetDemandById (int id);
+        Task<ICollection<Demand>> GetAllDemands();
+    }
+}
