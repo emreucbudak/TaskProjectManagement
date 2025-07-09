e3c22a7bae6b8acadc1b37539a115532f620d407
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-07-09 22:45:24 +0300

    TeamRepository Eklendi

---
+++ Infrastructure/TaskProjectManagement.Persistence/Repositories/TeamRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskProjectManagement.Application.Interfaces.Repositories;
+using TaskProjectManagement.Domain.Entities;
+using TaskProjectManagement.Persistence.ProjectDbContext;
+
+namespace TaskProjectManagement.Persistence.Repositories
+{
+    public class TeamRepository : RepositoryBase<Team>, ITeamRepository
+    {
+        public TeamRepository(ApplicationDbContext db) : base(db)
+        {
+        }
+
+        public async Task AddTeam(Team team)
+        {
+            await AddObj(team);
+        }
+
+        public async Task<IEnumerable<Team>> GetAllTeams()
+        {
+            return await GetAllObj(false);
+        }
+
+        public async Task<Team> GetTeamById(int id)
+        {
+            return await GetByIdObj(b => b.TeamId == id);
+        }
+
+        public async Task RemoveTeam(Team team)
+        {
+            await DeleteObj(team);
+        }
+
+        public async Task UpdateTeam(Team team)
+        {
+            await UpdateObj(team);
+        }
+    }
+}
