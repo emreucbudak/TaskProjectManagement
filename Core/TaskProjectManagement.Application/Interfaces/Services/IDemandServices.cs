da28abb227885848a35bee8fa7b61efdc4deb8c8
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-07-10 10:14:30 +0300

    IDemandService interfacei eklendi

--- Core/TaskProjectManagement.Application/Interfaces/Services/IDemandServices.cs
+++ Core/TaskProjectManagement.Application/Interfaces/Services/IDemandServices.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskProjectManagement.Domain.Entities;
 
 namespace TaskProjectManagement.Application.Interfaces.Services
 {
     public interface IDemandServices
     {
+        Task AddDemandFromService(Demand dem);
+        Task DeleteDemandFromService (int demandId);
+        Task<Demand> GetOneDemandFromService(int id);
+        Task<IEnumerable<Demand>> GetAllDemandFromService(bool v);
+        Task UpdateDemandFromService  (Demand demand);
     }
 }
