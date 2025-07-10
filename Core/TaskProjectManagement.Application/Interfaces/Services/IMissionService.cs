de72f70a0feca2de7a3e52efd19644615bc063b8
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-07-10 10:44:22 +0300

    MissionService İnterfacei eklendi

--- Core/TaskProjectManagement.Application/Interfaces/Services/IMissionService.cs
+++ Core/TaskProjectManagement.Application/Interfaces/Services/IMissionService.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskProjectManagement.Domain.Entities;
 
 namespace TaskProjectManagement.Application.Interfaces.Services
 {
     public interface IMissionService
     {
+        Task AddMission(Mission miss);
+        Task RemoveMission(int miss);
+        Task UpdateMigration (Mission miss);
+        Task<Mission> GetMissionByIdFromService(int id);
+        Task<IEnumerable<Mission>> GetAllMissions();
     }
 }
