6f4f4f5baa4810cd3d75cfa0b9cde53e33ca4b16
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-07-10 10:50:40 +0300

    Subtask servisi interfacei eklendi

--- Core/TaskProjectManagement.Application/Interfaces/Services/ISubtaskServices.cs
+++ Core/TaskProjectManagement.Application/Interfaces/Services/ISubtaskServices.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskProjectManagement.Domain.Entities;
 
 namespace TaskProjectManagement.Application.Interfaces.Services
 {
     public interface ISubtaskServices
     {
+        Task AddSubTaskFromService (SubTask subTask);
+        Task RemoveSubTaskFromService(int id);
+        Task UpdateSubTaskFromService(SubTask subTask);
+        Task<SubTask> GetSubTaskFromService (int id);
+        Task<IEnumerable<SubTask>> GetAllSubTasks ();
+
     }
+
 }
