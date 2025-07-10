e38d254c715a36a0b0896d73d5975ef2a51ff89b
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-07-10 10:48:12 +0300

    Notifikation servisi interfacei eklendi

--- Core/TaskProjectManagement.Application/Interfaces/Services/INotificationsService.cs
+++ Core/TaskProjectManagement.Application/Interfaces/Services/INotificationsService.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskProjectManagement.Domain.Entities;
 
 namespace TaskProjectManagement.Application.Interfaces.Services
 {
     public interface INotificationsService
     {
+        Task AddNotificationFromService(Notifications ntfc);
+        Task RemoveNotificationFromService(int ntfc);
+        Task UpdateNotificationFromService(Notifications ntfc);
+        Task<Notifications> GetNotificationsFromService();
+        Task<IEnumerable<Notifications>> GetAllNotificationsFromService();
     }
 }
