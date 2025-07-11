6489254bfe7160cb50651da548c69c8414b6362e
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-07-11 17:37:31 +0300

    Mission controllerı eklendi

---
+++ TaskProjectManagement/Controllers/MissionsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TaskProjectManagement.Domain.Entities;
+using TaskProjectManagement.Persistence.ProjectDbContext;
+
+namespace TaskProjectManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MissionsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MissionsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Missions
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Mission>>> GetMissions()
+        {
+            return await _context.Missions.ToListAsync();
+        }
+
+        // GET: api/Missions/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Mission>> GetMission(int id)
+        {
+            var mission = await _context.Missions.FindAsync(id);
+
+            if (mission == null)
+            {
+                return NotFound();
+            }
+
+            return mission;
+        }
+
+        // PUT: api/Missions/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMission(int id, Mission mission)
+        {
+            if (id != mission.MissionId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(mission).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MissionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Missions
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Mission>> PostMission(Mission mission)
+        {
+            _context.Missions.Add(mission);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetMission", new { id = mission.MissionId }, mission);
+        }
+
+        // DELETE: api/Missions/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMission(int id)
+        {
+            var mission = await _context.Missions.FindAsync(id);
+            if (mission == null)
+            {
+                return NotFound();
+            }
+
+            _context.Missions.Remove(mission);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool MissionExists(int id)
+        {
+            return _context.Missions.Any(e => e.MissionId == id);
+        }
+    }
+}
