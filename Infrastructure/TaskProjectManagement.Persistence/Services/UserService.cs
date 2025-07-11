0ab20ed01d178d94d3d3196e1b9b14f62cdc6f03
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-07-11 10:49:41 +0300

    User servis eklendi

--- Infrastructure/TaskProjectManagement.Persistence/Services/UserService.cs
+++ Infrastructure/TaskProjectManagement.Persistence/Services/UserService.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskProjectManagement.Application.Interfaces.Services;
+using TaskProjectManagement.Domain.Entities;
 
 namespace TaskProjectManagement.Persistence.Services
 {
-    internal class UserService
+    public class UserService : IUserServices
     {
+        public Task AddUserFromService(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteUserFromService(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<User>> GetAllUsers(bool v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetUserById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateUserFromService(User user)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
