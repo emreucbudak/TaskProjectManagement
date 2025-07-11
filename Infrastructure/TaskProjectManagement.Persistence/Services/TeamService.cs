3aea6e97fc5a7b636008ec82781d53fd16567269
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-07-11 10:42:58 +0300

    TEAM ARABİRİMİ UYGULANDI

--- Infrastructure/TaskProjectManagement.Persistence/Services/TeamService.cs
+++ Infrastructure/TaskProjectManagement.Persistence/Services/TeamService.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskProjectManagement.Application.Interfaces.Services;
+using TaskProjectManagement.Domain.Entities;
 
 namespace TaskProjectManagement.Persistence.Services
 {
-    internal class TeamService
+    public class TeamService : ITeamService
     {
+        public Task AddTeam()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Team>> GetAllTeams(bool v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Team> GetTeamById(int teamId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveTeam(int teamId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateTeamFromService(Team team)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
