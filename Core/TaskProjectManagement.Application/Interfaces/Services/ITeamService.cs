2678de1d666b526e6e0a16a22bbbcb44f04207b3
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-07-11 10:42:26 +0300

    Team SERVICE INTERFACEI DOLDURULDU

--- Core/TaskProjectManagement.Application/Interfaces/Services/ITeamService.cs
+++ Core/TaskProjectManagement.Application/Interfaces/Services/ITeamService.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskProjectManagement.Domain.Entities;
 
 namespace TaskProjectManagement.Application.Interfaces.Services
 {
     public interface ITeamService
     {
+        Task AddTeam();
+        Task RemoveTeam(int teamId);
+        Task<IEnumerable<Team>> GetAllTeams(bool v);
+        Task<Team> GetTeamById(int teamId);
+        Task UpdateTeamFromService(Team team);
     }
 }
