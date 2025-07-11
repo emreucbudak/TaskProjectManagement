ccc8f3439095e0579af120de31686802493b8f23
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-07-11 10:44:58 +0300

    user notifications interfacei eklendi

--- Core/TaskProjectManagement.Application/Interfaces/Services/IUserNotificationsServices.cs
+++ Core/TaskProjectManagement.Application/Interfaces/Services/IUserNotificationsServices.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskProjectManagement.Domain.Entities;
 
 namespace TaskProjectManagement.Application.Interfaces.Services
 {
     public interface IUserNotificationsServices
     {
+        Task addUserNotificationFromService (UserNotification userNotification);
+        Task removeUserNotificationFromService(int id);
+        Task updateUserNotificationFromService(UserNotification usr);
+        Task<UserNotification> getUserNotificationFromService (int id);
+        Task<IEnumerable<UserNotification>> getUserNotifications(bool v);
     }
 }
