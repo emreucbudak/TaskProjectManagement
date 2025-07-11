ea1a69fcd4979513fad0f20098e16fd8cb0f3f8a
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-07-11 10:46:52 +0300

    User service ınterfacei eklendi

--- Core/TaskProjectManagement.Application/Interfaces/Services/IUserServices.cs
+++ Core/TaskProjectManagement.Application/Interfaces/Services/IUserServices.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskProjectManagement.Domain.Entities;
 
 namespace TaskProjectManagement.Application.Interfaces.Services
 {
     public interface IUserServices
     {
+        Task AddUserFromService(User user);
+        Task UpdateUserFromService(User user);
+        Task DeleteUserFromService(int id);
+        Task<IEnumerable<User>> GetAllUsers(bool v);
+        Task<User> GetUserById(int id);
     }
 }
