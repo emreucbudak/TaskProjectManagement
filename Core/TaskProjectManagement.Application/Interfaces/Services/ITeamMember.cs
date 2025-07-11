d14453ebc0dff0ac411b412929c38712093b12fe
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-07-11 10:27:12 +0300

    ITeamMember interfaceinin ici  dolduruldu

--- Core/TaskProjectManagement.Application/Interfaces/Services/ITeamMember.cs
+++ Core/TaskProjectManagement.Application/Interfaces/Services/ITeamMember.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskProjectManagement.Domain.Entities;
 
 namespace TaskProjectManagement.Application.Interfaces.Services
 {
     public interface ITeamMember
     {
+        Task AddMemberFromService(TeamMember teamMember);
+        Task DeleteMemberFromService(int id);
+        Task UpdateMemberFromService(TeamMember member);
+        Task<TeamMember> GetMemberFromService(int id);
+        Task<IEnumerable<TeamMember>> GetAllMembersFromService(bool v);
     }
 }
