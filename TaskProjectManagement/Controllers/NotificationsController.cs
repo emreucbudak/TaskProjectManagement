a942995a0e1502f3cf236029fc4609e1611e25a1
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-07-12 00:25:59 +0300

    NotificationsControlleri içi dolduruldu

--- TaskProjectManagement/Controllers/NotificationsController.cs
+++ TaskProjectManagement/Controllers/NotificationsController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using TaskProjectManagement.Application.Interfaces.Services;
 using TaskProjectManagement.Domain.Entities;
 using TaskProjectManagement.Persistence.ProjectDbContext;
 
@@ -14,9 +15,9 @@ namespace TaskProjectManagement.Controllers
     [ApiController]
     public class NotificationsController : ControllerBase
     {
-        private readonly ApplicationDbContext _context;
+        private readonly IServiceManager _context;
 
-        public NotificationsController(ApplicationDbContext context)
+        public NotificationsController(IServiceManager context)
         {
             _context = context;
         }
@@ -25,52 +26,26 @@ namespace TaskProjectManagement.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Notifications>>> GetNotifications()
         {
-            return await _context.Notifications.ToListAsync();
+            var allNotifications =  await _context.notificationsService.GetAllNotificationsFromService(false);
+            return Ok(allNotifications);
         }
 
         // GET: api/Notifications/5
         [HttpGet("{id}")]
         public async Task<ActionResult<Notifications>> GetNotifications(int id)
         {
-            var notifications = await _context.Notifications.FindAsync(id);
-
-            if (notifications == null)
-            {
-                return NotFound();
-            }
-
-            return notifications;
+            return Ok(await _context.notificationsService.GetNotificationsFromService(id));
+ 
         }
 
         // PUT: api/Notifications/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutNotifications(int id, Notifications notifications)
+        public async Task<IActionResult> PutNotifications(Notifications notifications)
         {
-            if (id != notifications.NotificationId)
-            {
-                return BadRequest();
-            }
+            await _context.notificationsService.UpdateNotificationFromService(notifications);
+            return Ok(notifications);
 
-            _context.Entry(notifications).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!NotificationsExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
         }
 
         // POST: api/Notifications
@@ -78,8 +53,7 @@ namespace TaskProjectManagement.Controllers
         [HttpPost]
         public async Task<ActionResult<Notifications>> PostNotifications(Notifications notifications)
         {
-            _context.Notifications.Add(notifications);
-            await _context.SaveChangesAsync();
+            await _context.notificationsService.AddNotificationFromService(notifications);
 
             return CreatedAtAction("GetNotifications", new { id = notifications.NotificationId }, notifications);
         }
@@ -88,21 +62,11 @@ namespace TaskProjectManagement.Controllers
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteNotifications(int id)
         {
-            var notifications = await _context.Notifications.FindAsync(id);
-            if (notifications == null)
-            {
-                return NotFound();
-            }
-
-            _context.Notifications.Remove(notifications);
-            await _context.SaveChangesAsync();
+            await _context.notificationsService.RemoveNotificationFromService(id);
 
             return NoContent();
         }
 
-        private bool NotificationsExists(int id)
-        {
-            return _context.Notifications.Any(e => e.NotificationId == id);
-        }
+
     }
 }
