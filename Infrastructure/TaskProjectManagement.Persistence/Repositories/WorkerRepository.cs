9c763296351a72efa5f5242e49f3768af53b70ca
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-07-12 16:00:30 +0300

    Worker Repository Eklendi

---
+++ Infrastructure/TaskProjectManagement.Persistence/Repositories/WorkerRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskProjectManagement.Application.Interfaces.Repositories;
+using TaskProjectManagement.Domain.Entities;
+
+namespace TaskProjectManagement.Persistence.Repositories
+{
+    public class WorkerRepository : RepositoryBase<Worker>, IWorkerRepository
+    {
+        
+        public async Task AddWorker(Worker worker)
+        {
+           await AddObj(worker);
+            
+        }
+
+        public async Task DeleteWorker(Worker worker)
+        {
+            await DeleteObj(worker);
+        }
+
+        public async Task<IEnumerable<Worker>> GetAllWorkers()
+        {
+            return await GetAllObj(false);
+        }
+
+        public async Task<Worker> GetWorker(int worker)
+        {
+            return await GetByIdObj(b => b.Id == worker);
+
+        }
+
+        public async Task UpdateWorker(Worker worker)
+        {
+            await UpdateObj(worker);
+        }
+    }
+}
