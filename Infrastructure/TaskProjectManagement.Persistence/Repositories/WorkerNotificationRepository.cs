400a8f7cff063740d04c93a2d751ff006b232502
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-07-12 16:41:11 +0300

    WORKER NOTİFİCATİON REPOSİTORY EKLENDİ

---
+++ Infrastructure/TaskProjectManagement.Persistence/Repositories/WorkerNotificationRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskProjectManagement.Application.Interfaces.Repositories;
+using TaskProjectManagement.Domain.Entities;
+using TaskProjectManagement.Persistence.ProjectDbContext;
+
+namespace TaskProjectManagement.Persistence.Repositories
+{
+    public class WorkerNotificationRepository : RepositoryBase<WorkerNotification>, IWorkerNotificationRepository
+    {
+        public WorkerNotificationRepository(ApplicationDbContext db) : base(db)
+        {
+        }
+
+        public async Task AddWorkerNotification(int userId, int notificationId)
+        {
+            var addWorker = new WorkerNotification
+            {
+                NotificationsId = notificationId,
+                UserId = userId,
+            };
+            await AddObj(addWorker);
+        }
+
+        public async Task<IEnumerable<WorkerNotification>> getAllNotification(int userId)
+        {
+            var allNotification =  await GetAllObj(false);
+            return allNotification.Where(b  => b.UserId == userId);
+        }
+    }
+}
