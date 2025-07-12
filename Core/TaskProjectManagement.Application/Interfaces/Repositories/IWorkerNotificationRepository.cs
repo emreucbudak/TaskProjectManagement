b298bdce5000750ed91dc1a1ce9479c153efbe99
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-07-12 15:54:02 +0300

    Workernotification repository eklendi

---
+++ Core/TaskProjectManagement.Application/Interfaces/Repositories/IWorkerNotificationRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskProjectManagement.Domain.Entities;
+
+namespace TaskProjectManagement.Application.Interfaces.Repositories
+{
+    public interface IWorkerNotificationRepository
+    {
+        Task AddWorkerNotification(int userId, int notificationId);
+        Task<IEnumerable<WorkerNotification>> getAllNotification(int userId);
+        
+
+    }
+
+}
